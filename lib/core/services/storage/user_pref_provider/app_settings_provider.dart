684a25a567a1bdc67cc4ce35087d5481a09aaae1
zx1360 <3039328600@qq.com>
2025-10-12 12:26:41 +0800

    重构routine前备份.

--- lib/core/services/storage/user_pref_provider/app_settings_provider.dart
+++ lib/core/services/storage/user_pref_provider/app_settings_provider.dart
@@ -11,17 +11,29 @@ part 'app_settings_provider.g.dart';
 class AppSettingsProvider extends _$AppSettingsProvider {
   @override
   AppSettings build() {
-    return AppSettings();
+    final prefs = PrefsService.prefs;
+    final stringPrefs = prefs.getString("preferences");
+    return AppSettings.fromJson(jsonDecode(stringPrefs!));
   }
 
-  Future<void> loadSettings() async {
-    final prefs = await PrefsService.prefs;
+  // 初始化, 如果记录中没有值就赋予初值.
+  Future<void> initSettings()async{
+    final prefs = PrefsService.prefs;
+    if(!prefs.containsKey("preferences")){
+      state = AppSettings.defaultValue();
+      await saveSettings();
+    }
+  }
+
+  // 存/读
+  void loadSettings() {
+    final prefs = PrefsService.prefs;
     final stringPrefs = prefs.getString("preferences");
-    state = AppSettings.fromJson(jsonDecode(stringPrefs ?? ""));
+    state = AppSettings.fromJson(jsonDecode(stringPrefs!));
   }
 
   Future<void> saveSettings() async {
-    final prefs = await PrefsService.prefs;
+    final prefs = PrefsService.prefs;
     await prefs.setString("preferences", jsonEncode(state.toJson()));
   }
 }
