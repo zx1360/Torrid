6d1cbcf4ead766aeb0feb30086a1d58c21b8f370
zx1360 <3039328600@qq.com>
2025-12-25 18:16:54 +0800

    tuntun再放一放. 现在开始使用公共api加入到reader阅读页.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:torrid/app/app.dart';
 
 void main() async {
-  // // 1.确保Flutter绑定初始化
+  // // 确保Flutter绑定初始化
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(ProviderScope(child: const MyApp()));
